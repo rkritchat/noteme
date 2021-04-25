8ea2e1f6ce3ec11341945eed2cbe4bf0ed4fc94b
rkritchat <kritchat.r@kasikornline.com>
2021-04-25 19:59:41 +0700

    add router group

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,12 +3,14 @@ package router
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"net/http"
 	"noteme/internal/service"
 )
 
 func NewRouter(userService service.User) *fiber.App {
 	app := fiber.New()
 	app.Use(logger.New())
-	app.Get("/test", userService.GetUser)
+	group := app.Group("/user")
+	group.Add(http.MethodGet, "/test", userService.GetUser)
 	return app
 }
